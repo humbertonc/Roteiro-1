bc80a139a791a6043a3a27ae1deac3e4eecf6b8a
Humberto Navarro <54050212+humbertonc@users.noreply.github.com>
2019-11-30 22:58:48 -0300

    Create ControleDeGastos.h

---
+++ ControleDeGastos.h
@@ -0,0 +1,23 @@
+#ifndef CONTROLEDEGASTOS_H
+#define CONTROLEDEGASTOS_H
+#include "Despesa.h"
+
+
+class ControleDeGastos
+{
+    public:
+
+        ControleDeGastos();
+        virtual ~ControleDeGastos();
+        int calculaTotalDeDespesas();
+        bool existeDespesaDoTipo(string tipo);
+        void setDespesa(Despesa d, int pos);
+        Despesa getDespesa(int pos);
+
+    protected:
+
+    private:
+        Despesa despesas[100];
+};
+
+#endif // CONTROLEDEGASTOS_H
