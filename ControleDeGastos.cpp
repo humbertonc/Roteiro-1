b0b0b408c9fc18aaecc59d1ac4a5e1b2ba5e11cc
Humberto Navarro <54050212+humbertonc@users.noreply.github.com>
2019-11-30 22:59:08 -0300

    Create ControleDeGastos.cpp

---
+++ ControleDeGastos.cpp
@@ -0,0 +1,51 @@
+#include "ControleDeGastos.h"
+#include "Despesa.h"
+
+ControleDeGastos::ControleDeGastos()
+{
+    //ctor
+}
+
+void ControleDeGastos::setDespesa(Despesa d, int pos)
+{
+    despesas[pos] = d;
+}
+
+Despesa ControleDeGastos::getDespesa(int pos)
+{
+    return despesas[pos];
+}
+
+int ControleDeGastos::calculaTotalDeDespesas()
+{
+    int i, soma = 0;
+
+    for(i = 0; i < 100; i++){
+        if(despesas[i].getTipoDeGasto()[0] == '\0')
+        break;
+
+        soma += despesas[i].getValor();
+
+    }
+
+    return soma;
+}
+
+bool ControleDeGastos::existeDespesaDoTipo(string tipo)
+{   int i;
+    for(i = 0; i < 100; i++){
+        if(despesas[i].getTipoDeGasto()[0] == '\0')
+        break;
+
+        if(tipo == despesas[i].getTipoDeGasto())
+        return true;
+    }
+
+    return false;
+}
+
+
+ControleDeGastos::~ControleDeGastos()
+{
+    //dtor
+}
